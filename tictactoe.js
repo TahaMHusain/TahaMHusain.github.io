2fd56561033920ce0ad5c868b8c5a9d13152009d
Taha Husain <tahamh77@gmail.com>
2024-01-22 15:09:31 -0500

    added room joining and game phases

--- tictactoe.js
+++ tictactoe.js
@@ -8,19 +8,20 @@ let room;
 let sendResultValidation;
 let sendCellPlayed;
 let sendRestartGame;
+let statusDisplay;
 
 let numPlayers = 1;
 
-const statusDisplay = document.querySelector('.game--status');
+const container = document.getElementById("container");
+const contentBottom = document.getElementById("content-bottom");
 
+const MAX_PLAYERS = 2;
 const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 const config = {appId: 'taha-tictactoetest'};
 
 startup();
-
-statusDisplay.innerHTML = currentPlayerTurn();
 
 const winningConditions = [
     [0, 1, 2],
@@ -112,17 +113,16 @@ function handleRestartClick() {
     }
 }
 
+function checkRoom(roomCode) {
+    console.log("Attempting to join room " + roomCode + "...")
+    room = joinRoom(config, roomCode);
+    let peerList = room.getPeers();
 
-
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.querySelector('.game--restart').addEventListener('click', handleRestartClick);
-
-function startup() {
-    let getCellPlayed;
-    let getResultValidation;
-    let getRestartGame;
-    
-    room = joinRoom(config, 'yoyodyne');
+    if (peerList.size > MAX_PLAYERS - 1) {
+        console.log("Room "+ roomCode +" full! Exiting");
+        room.leave()
+        return false;
+    }
 
     room.onPeerJoin(peerId => {
         console.log(`${peerId} joined`);
@@ -132,6 +132,110 @@ function startup() {
         console.log(`${peerId} left`);
         numPlayers -= 1;
     });
+    return true;
+}
+
+function joinRoomFunc(roomCodeEl) {
+    console.log("In joinRoomFunc")
+    let roomCode = roomCodeEl.value;
+    
+    if (checkRoom(roomCode)) {
+        if (document.contains(document.getElementById("room-full-mssg"))) {
+            document.getElementById("room-full-mssg").remove();
+        };
+        startGame(); 
+    } else {
+        const roomFullMssg = document.createTextNode("Room " + roomCode + " is full! Try another code");
+        roomFullMssg.id = "room-full-mssg";
+        const container = document.getElementById("container");
+        container.appendChild(roomFullMssg);
+        document.body.insertBefore(roomFullMssg, container);
+    }
+}
+
+function createRoomFunc() {
+    console.log("Creating room...")
+    //Get random string for room code
+    let roomCode = (Math.random() + 1).toString(36).substring(7)
+
+    while (!(checkRoom(roomCode))) {
+        console.log("Room " + roomCode + "didn't work...")
+        roomCode = toString(parseInt(roomCode) + 1);
+    }
+
+    startGame();
+}
+
+function startup() {
+    const joinButton = document.createElement("button");
+    joinButton.className = "startup";
+    joinButton.textContent = "Join Room";
+    const spacer = document.createElement("br");
+    spacer.className = "startup";
+    const spacer2 = document.createElement("span");
+    spacer2.className = "startup";
+    spacer2.innerHTML = "<br><br><br>"
+    const roomCodeInput = document.createElement("input");
+    roomCodeInput.className = "startup";
+    roomCodeInput.textContent = "Room code";
+    const createRoomButton = document.createElement("button");
+    createRoomButton.className = "startup";
+    createRoomButton.textContent = "Create Room";
+
+    const container = document.getElementById("container");
+    container.appendChild(joinButton);
+    container.appendChild(spacer);
+    container.appendChild(spacer2);
+    container.appendChild(roomCodeInput);
+    container.appendChild(createRoomButton);
+
+    joinButton.addEventListener('click', () => joinRoomFunc(roomCodeInput));
+    createRoomButton.addEventListener('click', createRoomFunc);
+
+    container.insertBefore(roomCodeInput, contentBottom);
+    container.insertBefore(spacer, contentBottom);
+    container.insertBefore(joinButton, contentBottom);
+    container.insertBefore(spacer2, contentBottom);
+    container.insertBefore(createRoomButton, contentBottom);
+
+    
+
+}
+
+function startGame() {
+    console.log("Starting game...")
+    document.querySelectorAll('.startup').forEach(e => e.remove());
+
+    const gameHTML = document.createElement("span");
+    gameHTML.innerHTML = `
+    <section>
+        <div class="game--container">
+            <div data-cell-index="0" class="cell"></div>
+            <div data-cell-index="1" class="cell"></div>
+            <div data-cell-index="2" class="cell"></div>
+            <div data-cell-index="3" class="cell"></div>
+            <div data-cell-index="4" class="cell"></div>
+            <div data-cell-index="5" class="cell"></div>
+            <div data-cell-index="6" class="cell"></div>
+            <div data-cell-index="7" class="cell"></div>
+            <div data-cell-index="8" class="cell"></div>
+        </div>
+        <h2 class="game--status"></h2>
+        <button class="game--restart">Restart Game</button>
+    </section>
+    `
+    container.appendChild(gameHTML);
+    container.insertBefore(gameHTML, contentBottom);
+
+    document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+    document.querySelector('.game--restart').addEventListener('click', handleRestartClick);
+
+    statusDisplay = document.querySelector('.game--status');
+    statusDisplay.innerHTML = currentPlayerTurn();
+
+    let getCellPlayed;
+    let getResultValidation;
+    let getRestartGame;
 
     [sendCellPlayed, getCellPlayed] = room.makeAction('cellPlayed');
     [sendResultValidation, getResultValidation] = room.makeAction('resultV');
@@ -140,5 +244,4 @@ function startup() {
     getCellPlayed(handleCellPlayedPeer);
     getResultValidation(handleResultValidation);
     getRestartGame(handleRestartGame);
-
 }
